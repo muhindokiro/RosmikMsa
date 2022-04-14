bdbf54ad0d1731733a4043c60a415168b7066c43
muhindokiro <blaize1143@gmail.com>
2022-04-14 17:11:11 +0300

    Add Scripts files

---
+++ js/scripts.js
@@ -0,0 +1,15 @@
+// const loader = document.querySelector('.loader');
+// const body = document.querySelector('.body');
+
+// function init() {
+//     setTimeout(() => {
+//         loader.style.opacity = 0;
+//         loader.style.display = 'none';
+
+//     setTimeout(() => (body.style.opacity = 1), 50);
+//         body.style.display = 'block';
+
+//     }, 4000);
+// } 
+
+// init();
