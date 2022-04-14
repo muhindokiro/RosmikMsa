efeb5d382e3898b6c8ac4434382d09bbdc0cd9aa
muhindokiro <blaize1143@gmail.com>
2022-04-14 17:16:58 +0300

    Add moving motion to truck image

---
+++ js/custom.js
@@ -0,0 +1,46 @@
+
+ /* jQuery Pre loader
+  -----------------------------------------------*/
+$(window).load(function(){
+    $('.loader').fadeOut(4000); // set duration in brackets    
+});
+
+
+$(document).ready(function() {
+
+  /* Home Slideshow Vegas
+  -----------------------------------------------*/
+  $(function() {
+    $('body').vegas({
+        slides: [
+            { src: 'img/img3.jpeg' },
+            // { src: 'img/img1.jpg' }
+        ],
+        timer: false,
+        transition: [ 'zoomOut', ]
+    });
+  });
+
+
+   /* Back top
+  -----------------------------------------------*/
+    $(window).scroll(function() {
+        if ($(this).scrollTop() > 200) {
+        $('.go-top').fadeIn(200);
+        } else {
+          $('.go-top').fadeOut(200);
+        }
+        });   
+        // Animate the scroll to top
+      $('.go-top').click(function(event) {
+        event.preventDefault();
+      $('html, body').animate({scrollTop: 0}, 400);
+      })
+      
+
+  /* wow
+  -------------------------------*/
+  new WOW({ mobile: false }).init();
+
+  });
+
